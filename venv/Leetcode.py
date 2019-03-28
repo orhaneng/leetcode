e5d45d6ddc44fe571a654e6410ffb64376a83e23
Omer Orhan <omer.orhan@lexinsight.ai>
2019-03-27 19:48:54 -0700

    cc

--- venv/Leetcode.py
+++ venv/Leetcode.py
@@ -42,7 +42,35 @@ def getTopicFile(datamain):
             datamain.loc[datamain["LeetcodeId"]==item,"Topic"] = dict.get(item)
     datamain.to_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv",index=False)
     return datamain
-dftopics = getTopicFile(datamain)
+
+def getCompanyFile():
+    datamain = pd.read_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv", engine='python',
+                        index_col=False)
+    sheetnames = getSheetNames(excelfilecompany)
+    dfCompany = pd.DataFrame(columns=list(['LeetcodeId', 'Company']))
+    for sheet in sheetnames:
+        datacompany = readExcelRange(excelfilecompany,sheet,2,1000,1,1)
+        datatopic =  pd.DataFrame({'LeetcodeId':datacompany[:,0]})
+        for topicid in datatopic["LeetcodeId"]:
+            dfCompany = dfCompany.append({'LeetcodeId': topicid, "Company":sheet}, ignore_index=True)
+    dict = {};
+
+    for item in dfCompany.iterrows():
+        if item[1][0] in dict:
+            text = dict.get(item[1][0])
+            text = text + "|" + item[1][1]
+            dict.update({item[1][0]: text})
+        else:
+            dict.update({item[1][0]: item[1][1]})
+    print(dict)
+    for item in datamain["LeetcodeId"]:
+        if int(item) in dict:
+            datamain.loc[datamain["LeetcodeId"]==item,"Company"] = dict.get(item)
+    print(datamain)
+    datamain.to_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv",index=False)
+    return datamain
+getCompanyFile()
+#dftopics = getTopicFile(datamain)
 #dataset = pd.read_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv", engine='python',
 #                     index_col=False)
 #print(dataset)
