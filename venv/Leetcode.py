ba8e9758270ce122d1593e7dc72eafd530f15a49
Omer Orhan <omer.orhan@lexinsight.ai>
2019-03-27 18:38:46 -0700

    cc

--- venv/Leetcode.py
+++ venv/Leetcode.py
@@ -18,21 +18,15 @@ def getmainFile():
                             'Title':datamain[:,2],'Acceptance':datamain[:,3],'Difficulty':datamain[:,4],'Frequency':datamain[:,5]})
     return datamain
 datamain =getmainFile()
-#print(datamain["LeetcodeId"].to_frame())
-#print(type(datamain))
-#for item in datamain["LeetcodeId"]:
-#    print(item)
+
 #############
 
 def getTopicFile(datamain):
     sheetnames = getSheetNames(excelfiletopic)
-
     dftopics = pd.DataFrame(columns=list(['LeetcodeId', 'Topic']))
-
     for sheet in sheetnames:
         datatopic = readExcelRange(excelfiletopic,sheet,2,200,1,1)
         datatopic =  pd.DataFrame({'LeetcodeId':datatopic[:,0]})
-
         for topicid in datatopic["LeetcodeId"]:
             dftopics = dftopics.append({'LeetcodeId': topicid, "Topic":sheet}, ignore_index=True)
     dict = {};
@@ -43,12 +37,12 @@ def getTopicFile(datamain):
             dict.update({item[1][0]: text})
         else:
             dict.update({item[1][0]: item[1][1]})
-    print(dict)
-
-    #dftopics.to_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv",index=False)
-    return dftopics
-#dftopics = getTopicFile(datamain)
-
+    for item in datamain["LeetcodeId"]:
+        if item in dict:
+            datamain.loc[datamain["LeetcodeId"]==item,"Topic"] = dict.get(item)
+    datamain.to_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv",index=False)
+    return datamain
+dftopics = getTopicFile(datamain)
 #dataset = pd.read_csv("/Users/omerorhan/Desktop/UCSC/resume/leetcode/leetcode/venv/leetcoderesult.csv", engine='python',
 #                     index_col=False)
 #print(dataset)
